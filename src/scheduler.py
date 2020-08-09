e51feedb09cc912fa5e79acd5166f0f603382f32
Jerry Yang <jyang9@nd.edu>
2020-08-09 17:52:46 -0400

    create placeholder for scheduler and tripgenerator

---
+++ src/scheduler.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python
+# ConTraG, Contextual Trajectory Generator; see https://
+# Copyright (C) 2020-2020 University of Notre Dame and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file     scheduler.py
+# @author   Jian Yang
+# @date     2020-07-20
+
+"""
+Two major features are realized in this python script
+1) translate a raw schedule into formatted itinerary
+2) extract key info. from sample trajectory data and generate itinerary
+"""
+
+
+def read_raw_schedule():
+    pass
+
+
+def write_itinerary():
+    pass
+
+
+def generate_itinerary():
+    print(0)
+
+
+if __name__ == "__main__":
+    print(0)
